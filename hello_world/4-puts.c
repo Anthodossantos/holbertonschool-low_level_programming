59cedc1b2931d49425b75e048b05807296f4fe6e
Anthony <anthony.dossantos@gmx.com>
2022-06-08 18:58:17 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
-   puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+/**
+* main - void
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
