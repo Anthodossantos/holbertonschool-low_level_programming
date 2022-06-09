0418d4e79f119a290a741cb7ba115e07e162eb89
Anthony <anthony.dossantos@gmx.com>
2022-06-09 12:46:24 +0200

    print alphabet and remove letter q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 
- */
+*main - prints the alphabet in lowercase,
+*followed by a new line, except q and e
+*Return: Aways 0
+*/
 int main(void)
 {
 char ch = 'a';
