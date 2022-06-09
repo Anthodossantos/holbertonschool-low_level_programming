138528ae306c412518ce65d9557a3b19c0073996
Anthony <anthony.dossantos@gmx.com>
2022-06-09 10:53:30 +0200

    print alphabet and remove letter q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - putchar alphabet
+*
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+printf("%c", c);
+printf("\n");
+removeChar(str, 'e' 'q');
+return (0);
+}
