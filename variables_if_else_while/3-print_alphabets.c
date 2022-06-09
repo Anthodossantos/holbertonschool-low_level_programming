2657ead499b7ba334f672a7eabc0db3b93b37861
Anthony <anthony.dossantos@gmx.com>
2022-06-09 10:15:46 +0200

    print lower and uppercase alphabet putchar

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,9 @@
 int main(void)
 {
 char c;
-for (c = 'a'; c <= 'z'; c = 'A'; c <= 'Z'; ++c)
+for (c = 'a'; c <= 'z'; ++c)
+for (c = 'A'; c <= 'Z'; ++c)
 printf("%c", c);
+printf("\n");
 return (0);
 }
