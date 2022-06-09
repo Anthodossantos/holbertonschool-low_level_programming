2dd8b56b8e53f9975a2b32d253db3b4aa3ca55a4
Anthony <anthony.dossantos@gmx.com>
2022-06-09 14:56:03 +0200

    alphabet reverse with putchar

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase,
+*followed by a new line, except q and e
+*Return: Aways 0
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+putchar(ch, rev ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}
