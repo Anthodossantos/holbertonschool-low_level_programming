bc41fe0fe20341a27d3fae94bf00ffce15a02694
Anthony <anthony.dossantos@gmx.com>
2022-06-09 13:01:06 +0200

    update

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-int num = 1024;
+int num =012345678;
 while (num != 0)
 {
 int digit = num % 10;
 num = num / 10;
-printf("%d\n", digit);
+printf("%d", digit);
 }
 return (0);
 }
