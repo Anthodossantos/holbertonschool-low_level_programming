c8a5be0e98c05c45e3109c41a3f0bf11624cac64
Anthony <anthony.dossantos@gmx.com>
2022-06-10 14:26:06 +0200

    prints _putchar return 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base 10 starting from 0,
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf(_putchar'\n');
+return (0);
+}
