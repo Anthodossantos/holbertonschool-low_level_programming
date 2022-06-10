538a487d47efe55d1120c46e9e5a8a009e858930
Anthony <anthony.dossantos@gmx.com>
2022-06-10 11:12:24 +0200

    putchar number with , and space

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,16 +6,17 @@
 */
 int main(void)
 {
-int i;
-for (i = '0'; i <= '9'; i++)
-{
-putchar(i);
-putchar(',');
-putchar(' ');
-}
-if (i != 9)
-{
-putchar(i);
-}
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 9)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
 return (0);
 }
