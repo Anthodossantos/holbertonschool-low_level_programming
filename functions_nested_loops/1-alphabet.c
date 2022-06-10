c1b513d834b0f5cf027bad50b677619d1e402db6
Anthony <anthony.dossantos@gmx.com>
2022-06-10 15:43:22 +0200

    update

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase, with prototype "main.h"
+*
+*Return: Aways 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		printf("%i", i);
+		printf("\n");
+return (0);
+}
